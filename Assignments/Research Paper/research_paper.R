91bae24c98a33567aec047f9ff5b90e51dd0ca0d
evan-kramer <evan.kramer@tn.gov>
2019-04-02 16:53:29 -0500

    Added data sources

--- Assignments/Research Paper/research_paper.R
+++ Assignments/Research Paper/research_paper.R
@@ -7,34 +7,55 @@ options(java.parameters = "-Xmx16G")
 library(tidyverse)
 library(lubridate)
 library(haven)
+library(RSocrata)
 setwd("C:/Users/CA19130/Documents/Projects/ECON 6110/")
 
+# Config
+data = F
+clean = F
+analysis = F
+visuals = F
+if(!"pwd" %in% ls()) {
+  # em = rstudioapi::askForPassword("Email:")
+  # pwd = rstudioapi::askForPassword("Password:")  
+}
+
 # Data
-production = readxl::read_excel("Assignments/Assignment 3/Production data- Spring 2019.xlsx", 
-                                sheet = 2, skip = 1, n_max = 34) %>% 
-  janitor::clean_names() %>% 
-  mutate_all(log) 
+if(data) {
+  adm = as.tbl(openxlsx::read.xlsx("https://www.tn.gov/content/dam/tn/education/data/profile/district_profile_2017-18.xlsx")) %>% 
+    janitor::clean_names()
+  
+  # Enrollment
+  # Number of schools
+  # Per-pupil expenditures
+  # Amount of academic growth
+  # Proficiency rate
+  # Urbanicity-rurality
+  # County-level economic indicators (% with bachelors degree, % unemployment, median income, etc.)
+} else {
+  rm(data)
+}
 
-# Regression
-model = lm(output ~ ., production)
-summary(model)
-sum(model$coefficients)
+# Clean
+if(clean) {
+  
+} else {
+  rm(clean)
+}
 
-# Estimate output elasticities of the inputs. Do you believe these estimates?
-model$coefficients
+# Analysis
+if(analysis) {
+  # Model to predict crashes
+  summary(lm(number_of_injuries ~ number_of_motor_vehicles + weather + collision_type + illumination + precinct,
+             crashes))
+  
+} else {
+  rm(analysis)
+}
 
-# What is the returns-to-scale in this industry?
-case_when(
-  sum(model$coefficients) == 1 ~ "constant",
-  sum(model$coefficients) > 1 ~ "increasing",
-  sum(model$coefficients) < 1 ~ "decreasing"
-)
-
-# What might be the shape of the long-run average cost curve? 
-  # When is output expected to grow 10% per year for the next three years?
-  # May want to use average values of IVs as starting point
-  # Assuming one can buy as much skilled labor as possible at $40/hr and unskilled at $15/hr
-  # Also prices of raw material is $20/board-ft, $200/unit of energy
-# Suppose this industry is being considered for a takeover by a foreign company.
-
-  # What would the investors likely be doing in terms of better profitability and why? 
+# Visualizations
+if(visuals) {
+  
+} else {
+  rm(visuals)
+}
